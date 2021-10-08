813b8a2aac33aaae485db4785c4b307439c77294
YLF <1463020554@qq.com>
2021-10-08 08:39:34 +0800

    update CliEngine.js

--- test/js/dp.config.ubs.js
+++ test/js/dp.config.ubs.js
@@ -2,18 +2,18 @@ const baseConfig = require('./dp.config.json');
 
 const classifyConfig = {
     "classify":"ubs",
-    "sourceFile": "./files/otherMain.js",
-    "targetFile": "./files/main.js",
+    "sourceFile": "../files/otherMain.js",
+    "targetFile": "../files/main.js",
     "moduleConfig": [
         {
-            "filePath": "./files/children/file.js",
+            "filePath": "../files/children/file.js",
             "replaceModules": [
                 {
                     "source": "import C from './C';",
                     "target": "import C from './testC';"
                 },
                 {
-                    "source": "import D from './D';",
+                    "source": "import D from '../D';",
                     "target": "import D from './testD';"
                 }
             ]
